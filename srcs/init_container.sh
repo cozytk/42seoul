3dbc0e394578f3ec6c8d69384a1f0675ee5087aa
Taekyun Kim <taekkim@c2r7s3.42seoul.kr>
2020-07-30 20:46:00 +0900

    no input password, authority changed

--- srcs/init_container.sh
+++ srcs/init_container.sh
@@ -1,10 +1,8 @@
 service mysql start
 
-mysql < var/www/html/phpmyadmin/sql/create_tables.sql -u root --skip-password
-mysqladmin -u root -p password
-
 echo "CREATE DATABASE IF NOT EXISTS wordpress;" | mysql -u root --skip-password
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'%' identified by 'kchoi' WITH GRANT OPTION;" | mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
+echo "update mysql.user set plugin='mysql_native_password' where user='root';" | mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 
 service php7.3-fpm start
