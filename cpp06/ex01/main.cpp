7c579703960dfad4abda027209752abb5d47e678
taekkim <taekkim@student.42seoul.kr>
2021-04-04 18:13:22 +0900

    FIX : delete allocate data

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,20 +6,12 @@ int	main()
 	Data	*data;
 
 	srand(time(0));
-
 	ptr = serialize();
 	data = deserialize(ptr);
-
-	std::cout << "Data translates to :" << std::endl;
+	std::cout << "deserialize :" << std::endl;
 	std::cout << "s1 : " << data->s1 << std::endl;
 	std::cout << "n : " << data->n << std::endl;
 	std::cout << "s2 : " << data->s2 << std::endl;
-
-	delete data;
 	delete reinterpret_cast <Data *> (ptr);
-
-	while (1)
-		;
-
 	return (0);
 }
--- cpp06/ex01/serialize.cpp
+++ cpp06/ex01/serialize.cpp
@@ -6,18 +6,21 @@ void	*serialize(void)
 	std::string		s2;
 	int				n = rand();
 	std::string		chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+	Data			*data = new Data;
 
 	for ( int i = 0 ; i < 8 ; i++ )
 	{
 		s1 += chars[rand() % 36];
 		s2 += chars[rand() % 36];
 	}
-
-	Data			*data = new Data;
-
 	data->s1 = s1;
 	data->n = n;
 	data->s2 = s2;
+
+	std::cout << "serialize :" << std::endl;
+	std::cout << "s1 : " << data->s1 << std::endl;
+	std::cout << "n : " << data->n << std::endl;
+	std::cout << "s2 : " << data->s2 << std::endl;
 
 	return (reinterpret_cast <void *> (data));
 }
