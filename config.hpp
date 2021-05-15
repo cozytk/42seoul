05f676f465cd0be7fed117049c71d88e6610d287
Yaechan Hong <yahong@c4r1s7.42seoul.kr>
2021-05-15 17:10:27 +0900

    config

--- config.hpp
+++ config.hpp
@@ -1,10 +1,45 @@
 #ifndef CONFIG_HPP
 # define CONFIG_HPP
 
-class config
-{
-public:
+# include <iostream>
 
+/*
+ * tree 형태 
+ * _ 붙은 건 임시용 이름, 이후에 이름 바꿔야 함
+ * */
+
+namespace
+{
+
+	/*
+	 * server {
+	 *    new {
+	 *      giv a;
+	 *    }
+	 * }
+	 * 일 경우 config["server"]["new"]["giv"]로 사용 가능하게
+
+	class config
+	{
+	private:
+		//map<> new;
+	
+		// \" 같은 건 어떻게 처리할 건지
+		enum _deep
+		{
+			syntax,
+			string
+		}
+
+		int _bracket;	
+		
+	public:
+		config();
+		config(config const &x);
+		~config();
+
+		void read(std::string const &path);
+	}
 }
 
 #endif
