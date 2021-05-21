07d4a046b36e846d1b50f9272cfecafa62c1d1f6
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2021-05-21 13:48:01 +0900

    feat: Feat Reaquest parse head

---
+++ srcs/Request.hpp
@@ -0,0 +1,49 @@
+#ifndef REQUEST_HPP
+# define REQUEST_HPP
+
+# include <string>
+# include <map>
+# include <iostream>
+# include <vector>
+# include <utility>
+#include <cstring>
+#include <sstream>
+class Request
+{
+	public:
+	typedef	std::map<std::string, std::string> HeaderType;
+	private:
+		std::string	_version;
+		HeaderType	_headers;
+		std::string	_body;
+	public:
+		Request();
+		Request(std::string const &request);
+		Request(const Request& copy);
+		Request& operator=(const Request& obj);
+		virtual ~Request();
+
+		bool		isValid();
+		HeaderType	getHeaders();
+};
+//Accept-Charsets
+//Accept-Language
+//Allow
+//Authorization
+//Content-Language
+//Content-Length
+//Content-Location
+//Content-Type
+//Date
+//Host
+//Last-Modified
+//Location
+//Referer
+//Retry-After
+//Server
+//Transfer-Encoding
+//User-Agent
+//WWW-Authenticate
+
+
+#endif
