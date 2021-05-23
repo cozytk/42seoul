7f3e92ec4c0aee46a008e1809e37b6504e7be7b8
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2021-05-23 11:14:35 +0900

    test: Feat chuncked request script

---
+++ srcs/Connection/req.sh
@@ -0,0 +1,16 @@
+{echo "POST / HTTP/1.1
+Date: Fri, 31 Dec 1999 23:59:59 GMT
+Content-Type: text/plain
+Transfer-Encoding: chunked
+
+1a
+abcdefghijklmnopqrstuvwxyz
+";
+echo; sleep 1;
+} | telnet localhost 8080
+
+{echo "10
+1234567890123456
+0";
+echo; sleep 1;
+} | telnet localhost 8080
